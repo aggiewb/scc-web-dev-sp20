461efd12b2a0cea7363c8b83f75b3b14d1445f4c
AggieWB <usmaggie23@gmail.com>
2020-04-14 14:26:29 -0700

    Change variable to reflect correct attribute

--- itc155/Assignment0_Octagon/src/Octagon.java
+++ itc155/Assignment0_Octagon/src/Octagon.java
@@ -1,19 +1,24 @@
 
 public class Octagon implements Shape {
-	private int sides;
+	private int sideLength;
 	
-	public Octagon(int sides) {
-		this.sides = sides;
+	public Octagon(int sideLength) {
+		this.sideLength = sideLength;
 	}
 
 	@Override
 	public double getArea() {
-		return 2 * (1 + Math.sqrt(2)) * Math.pow(sides, 2);
+		return 2 * (1 + Math.sqrt(2)) * Math.pow(sideLength, 2);
 	}
 
 	@Override
 	public double getPerimeter() {
-		return 8 * sides;
+		return 8 * sideLength;
 	}
 
+	@Override
+	public String toString() {
+		return "One side of this octagon has a length of " + sideLength;
+	}
+	
 }
