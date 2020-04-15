5d6efe332b71ab1837dc993f78f1f0f773f827da
AggieWB <usmaggie23@gmail.com>
2020-04-15 08:54:50 -0700

    Add test methods for perimeter of small, med, and lg shapes

--- itc155/Assignment0_Octagon/src/ShapeTest.java
+++ itc155/Assignment0_Octagon/src/ShapeTest.java
@@ -36,5 +36,38 @@ class ShapeTest {
 		assertEquals(hexagon.getArea(), 25980.76211353316);
 		assertEquals(dodecagon.getArea(), 111961.52422706631);
 	}
+	
+	@Test
+	void testSmallPerimeter() {
+		Shape octagon = new Octagon(10);
+		Shape hexagon = new Hexagon(10);
+		Shape dodecagon = new Dodecagon(10);
+		
+		assertEquals(octagon.getPerimeter(), 80.0);
+		assertEquals(hexagon.getPerimeter(), 60.0);
+		assertEquals(dodecagon.getPerimeter(), 120.0);
+	}
+	
+	@Test
+	void testMediumPerimeter() {
+		Shape octagon = new Octagon(50);
+		Shape hexagon = new Hexagon(50);
+		Shape dodecagon = new Dodecagon(50);
+		
+		assertEquals(octagon.getPerimeter(), 400.0);
+		assertEquals(hexagon.getPerimeter(), 300.0);
+		assertEquals(dodecagon.getPerimeter(), 600.0);
+	}
+	
+	@Test
+	void testLargePerimeter() {
+		Shape octagon = new Octagon(100);
+		Shape hexagon = new Hexagon(100);
+		Shape dodecagon = new Dodecagon(100);
+		
+		assertEquals(octagon.getPerimeter(), 800.0);
+		assertEquals(hexagon.getPerimeter(), 600.0);
+		assertEquals(dodecagon.getPerimeter(), 1200.0);
+	}
 
 }
