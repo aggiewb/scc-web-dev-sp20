970da2fea25f2f24180f8765e9f07d8bae22bd7c
AggieWB <usmaggie23@gmail.com>
2020-04-22 14:15:56 -0700

    Add method doubleList to duplicate each String in an ArrayList
    Add a main method with an empty ArrayList, add strings to the ArrayList, calls to the doubleList method, and printing of the ArrayLists

--- itc155/Assignment3_ArrayList/src/Exercise_10_4.java
+++ itc155/Assignment3_ArrayList/src/Exercise_10_4.java
@@ -1,9 +1,20 @@
+import java.util.*;
 
 public class Exercise_10_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> list = new ArrayList<>();
+		list.add("how");
+		list.add("are");
+		list.add("you?");
+		System.out.println(list);
+		doubleList(list);
+		System.out.print(list);
 	}
-
+	
+	public static void doubleList(ArrayList<String> words){
+	    for(int i = 0; i < words.size(); i+=2){
+	        words.add(i + 1, words.get(i));
+	    }   
+	}
 }
