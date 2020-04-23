d14215bc7dfbed7f4c858eab7ca5d74bae51a90a
AggieWB <usmaggie23@gmail.com>
2020-04-23 09:07:34 -0700

    Add commenting to describe assignment requirements

---
+++ web150/assignment2/js/user_input.js
@@ -0,0 +1,3 @@
+/* Code a program that accepts user input for the name as well as two numbers to add. 
+Hint: Use prompt to get user input. You will need to store the user input in variables. 
+Prompt returns a string, so you will need to convert the numbers to integers using ParseInt. */
