37eac37929d8f54a12ca1f4b8910796473e2b3e4
AggieWB <usmaggie23@gmail.com>
2020-04-25 14:06:44 -0700

    Correct spelling error

--- web150/assignment2/js/user_input.js
+++ web150/assignment2/js/user_input.js
@@ -2,7 +2,7 @@
 Hint: Use prompt to get user input. You will need to store the user input in variables. 
 Prompt returns a string, so you will need to convert the numbers to integers using ParseInt. */
 
-//Program description, user primpts, and input of user information
+//Program description, user prompts, and input of user information
 var userName = window.prompt("Let's play the sum game. \n Please enter your name");
 var userFirstNum = window.prompt("Enter first number");
 var userSecondNum = window.prompt("Enter second number");
