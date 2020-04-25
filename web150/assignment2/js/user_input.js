c1d6cc224ff0eb943592a08693ee602b07aad86f
AggieWB <usmaggie23@gmail.com>
2020-04-25 10:44:58 -0700

    Change var names for better clarification

--- web150/assignment2/js/user_input.js
+++ web150/assignment2/js/user_input.js
@@ -3,9 +3,9 @@ Hint: Use prompt to get user input. You will need to store the user input in var
 Prompt returns a string, so you will need to convert the numbers to integers using ParseInt. */
 
 
-var getName = window.prompt("Let's play the sum game. Please enter your name.");
-var getFirstNum = window.prompt("Enter first number");
-var getSecondNum = window.prompt("Enter second number");
+var userName = window.prompt("Let's play the sum game. Please enter your name.");
+var userFirstNum = window.prompt("Enter first number");
+var userSecondNum = window.prompt("Enter second number");
 
 var sumGameMessage = document.getElementById("sum-game-message");
-sumGameMessage.textContent = "Hi " + getName + "! The sum of " + getFirstNum + " + " + getSecondNum + " is " + userSum;
+sumGameMessage.textContent = "Hi " + userName + "! The sum of " + userFirstNum + " + " + userSecondNum + " is " + userSum;
