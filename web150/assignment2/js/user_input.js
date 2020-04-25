4b1848da027201ba22df5295a5730d4a5d8b69d9
AggieWB <usmaggie23@gmail.com>
2020-04-25 10:36:22 -0700

    Add vars to store user input, and a var for a message to output

--- web150/assignment2/js/user_input.js
+++ web150/assignment2/js/user_input.js
@@ -1,3 +1,11 @@
 /* Code a program that accepts user input for the name as well as two numbers to add. 
 Hint: Use prompt to get user input. You will need to store the user input in variables. 
 Prompt returns a string, so you will need to convert the numbers to integers using ParseInt. */
+
+
+var getName = window.prompt("Let's play the sum game. Please enter your name.");
+var getFirstNum = window.prompt("Enter first number");
+var getSecondNum = window.prompt("Enter second number");
+
+var sumGameMessage = document.getElementById("sum-game-message");
+sumGameMessage.textContent = ;
