ef847116715e0f3a0c8eda19f8c08b59e0f94188
AggieWB <usmaggie23@gmail.com>
2020-04-25 10:50:20 -0700

    Add method to user num input to parse string to num

--- web150/assignment2/js/user_input.js
+++ web150/assignment2/js/user_input.js
@@ -2,11 +2,12 @@
 Hint: Use prompt to get user input. You will need to store the user input in variables. 
 Prompt returns a string, so you will need to convert the numbers to integers using ParseInt. */
 
-
+//Program description, user primpts, and input of user information
 var userName = window.prompt("Let's play the sum game. Please enter your name.");
-var userFirstNum = window.prompt("Enter first number");
-var userSecondNum = window.prompt("Enter second number");
+var userFirstNum = parseInt(window.prompt("Enter first number"));
+var userSecondNum = parseInt(window.prompt("Enter second number"));
 var userSum = userFirstNum + userSecondNum;
 
+//Output of user information to html element
 var sumGameMessage = document.getElementById("sum-game-message");
 sumGameMessage.textContent = "Hi " + userName + "! The sum of " + userFirstNum + " + " + userSecondNum + " is " + userSum;
