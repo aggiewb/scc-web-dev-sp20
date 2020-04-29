bd47e9f697d4c03b3cbc9f4f96b01bf9112372d7
AggieWB <usmaggie23@gmail.com>
2020-04-29 11:07:35 -0700

    Add method that assess if two keys map to the same value
    Import class java library

--- itc155/Assignment4_Collections/src/Unique.java
+++ itc155/Assignment4_Collections/src/Unique.java
@@ -1,4 +1,18 @@
+import java.util.*;
 
 public class Unique {
-
+	public static boolean isUnique(Map<String, String> names){
+	    Collection<String> firstNames = names.values();
+	    Set<String> uniqueNames = new HashSet<>();
+	    
+	    for(String name : firstNames){
+	        uniqueNames.add(name);
+	    }
+	    
+	    if(uniqueNames.size() < names.size()){
+	        return false;
+	    }
+	   
+	    return true;
+	}
 }
