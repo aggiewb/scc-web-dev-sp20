3a29fdf83527ccc97b7f5cb5a324e8e12f4b3b2c
AggieWB <usmaggie23@gmail.com>
2020-04-29 19:09:16 -0700

    Add semicolons and add description to comment

--- web150/assignment3/js/object.js
+++ web150/assignment3/js/object.js
@@ -7,7 +7,7 @@ The object must have a minimum of 2 properties and 1 method. 25 points
 Display the properties, values, and method(s) that result, on a web page using the textContent or InnerHTML properties.  25 points
 */
 
-//Profile object constructor
+//Dating profile object constructor
 function Profile(name, age, location){
     this.name = name;
     this.age = age;
@@ -15,8 +15,9 @@ function Profile(name, age, location){
     this.totalLikes = 0;
     this.like = function(){
         this.like++;
-    }
+    };
     this.dislike = function(){
         this.like--;
-    }
+    };
 }
+
