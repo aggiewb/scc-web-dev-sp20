724aa68ec88d7c4b1b43a90188682e340c50d8e0
AggieWB <usmaggie23@gmail.com>
2020-04-30 08:18:29 -0700

    Remove punctuation in comments

--- web150/assignment3/js/object.js
+++ web150/assignment3/js/object.js
@@ -1,8 +1,8 @@
 /* For this assignment you will model an object of your choosing and interest and code it in JavaScript. 
-For example, you could code a Student, Product object, etc… Objects we coded in class will not count.
+For example, you could code a Student, Product object, etc Objects we coded in class will not count.
 
 Requirements:
-Use the constructor notation to create a “template” for the object. 25 points
+Use the constructor notation to create a template for the object. 25 points
 Create at least 2 instances of the object. 25 points
 The object must have a minimum of 2 properties and 1 method. 25 points
 Display the properties, values, and method(s) that result, on a web page using the textContent or InnerHTML properties.  25 points
