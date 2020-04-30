e44e4ec2a1d447e825d39f4494907dcaa78503f6
AggieWB <usmaggie23@gmail.com>
2020-04-29 19:04:14 -0700

    Add object contructor for dating profile

--- web150/assignment3/js/object.js
+++ web150/assignment3/js/object.js
@@ -6,3 +6,17 @@ Create at least 2 instances of the object. 25 points
 The object must have a minimum of 2 properties and 1 method. 25 points
 Display the properties, values, and method(s) that result, on a web page using the textContent or InnerHTML properties.  25 points
 */
+
+//Profile object constructor
+function Profile(name, age, location){
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.totalLikes = 0;
+    this.like = function(){
+        this.like++;
+    }
+    this.dislike = function(){
+        this.like--;
+    }
+}
