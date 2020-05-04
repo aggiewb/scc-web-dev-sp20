c9c2c5dc1961d2233b653aa5f62f8dc8e45b6f6e
AggieWB <usmaggie23@gmail.com>
2020-05-04 13:50:15 -0700

    Change html tag to footer for better HTML5 ref
    Change getElementByID() to querySelector() for footer element

--- web150/week4/documentObject/js/document-object.js
+++ web150/week4/documentObject/js/document-object.js
@@ -9,7 +9,7 @@ msg += '<b>page address: </b>' + document.URL + '<br />';
 msg += '<b>last modified: </b>' + document.lastModified + '</p>';
  
 // Create a variable called el to hold the element whose id attribute has a value of footer
-var el = document.getElementById('footer');
+var el = document.querySelector('footer');
  
 // Write the message into that element
 el.innerHTML = msg;
