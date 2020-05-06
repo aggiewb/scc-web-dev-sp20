8ce5154a71bbc1428ab8deb88ec392b211b56e2b
AggieWB <usmaggie23@gmail.com>
2020-05-06 16:09:12 -0700

    Add test methods to assert console output
    Add test method to assert thrown IllegalArgumentException
    Add comments to explain Java lambda expression, assertThrows, ByteArrayOutputStream, java.lang.System.setOut(), and java.io.ByteArrayOutputStream.toString()

--- itc155/Assignment5_Recursion/src/JTestIsNumbers.java
+++ itc155/Assignment5_Recursion/src/JTestIsNumbers.java
@@ -1,31 +1,57 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 class JTestIsNumbers {
 	@Test
 	void testZero() {
-		fail("Not yet implemented");
+		// () -> A Java lambda expression can be passed around as if it was an object and executed on demand.
+		// assertThrows(Class<T> expectedType, Executable executable) <T> means Generic class
+		assertThrows(IllegalArgumentException.class, () -> Numbers.writeNums(0));
 	}
 
 	@Test
 	void testOne() {
-		fail("Not yet implemented");
+		// Create a ByteArrayOutputStream, which is a new byte array output stream,
+		// to get the output from the call to print
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		// Change System.out to point out to stream
+		// java.lang.System.setOut() method reassigns the "standard" output stream
+		System.setOut(new PrintStream(outContent));
+		
+		Numbers.writeNums(1);
+		
+		// java.io.ByteArrayOutputStream.toString() method converts the buffer's contents into a string 
+		// decoding bytes using the platform's default character set.
+		assertEquals("1", outContent.toString());
 	}
 	
 	@Test
 	void testTwo() {
-		fail("Not yet implemented");
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+				
+		Numbers.writeNums(2);
+		assertEquals("1, 2", outContent.toString());
 	}
 	
 	@Test
 	void testFive() {
-		fail("Not yet implemented");
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));;
+				
+		Numbers.writeNums(5);
+		assertEquals("1, 2, 3, 4, 5", outContent.toString());
 	}
 	
 	@Test
 	void testTwelve() {
-		fail("Not yet implemented");
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		Numbers.writeNums(12);
+		assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12", outContent.toString());
 	}
 	
 }
