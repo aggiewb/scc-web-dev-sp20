c6cbe4979813c8fb4c8de3d96c7e5408ffe1cee8
AggieWB <usmaggie23@gmail.com>
2020-05-06 12:08:50 -0700

    Change names of test methods to reflect each test input

--- itc155/Assignment5_Recursion/src/JTestIsNumbers.java
+++ itc155/Assignment5_Recursion/src/JTestIsNumbers.java
@@ -3,30 +3,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class JTestIsNumbers {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void test() {
+	void testZero() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testOne() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testTwo() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testFive() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testTwelve() {
+		fail("Not yet implemented");
+	}
+	
 }
