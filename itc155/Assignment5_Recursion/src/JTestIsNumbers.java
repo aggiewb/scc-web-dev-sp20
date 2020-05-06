1be4b7b75f898f85f618fcab5b65029de9b9107d
AggieWB <usmaggie23@gmail.com>
2020-05-06 11:57:33 -0700

    Add new JUnit test case, and add 5 test case methods

---
+++ itc155/Assignment5_Recursion/src/JTestIsNumbers.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JTestIsNumbers {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
