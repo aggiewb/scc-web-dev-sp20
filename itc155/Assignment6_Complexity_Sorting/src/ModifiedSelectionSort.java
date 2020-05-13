c33c748e0625700b9dfe52658c161e262e9198cb
AggieWB <usmaggie23@gmail.com>
2020-05-13 10:59:37 -0700

    Add class for assignment 6 for a modified selection sort
    Add comment description

---
+++ itc155/Assignment6_Complexity_Sorting/src/ModifiedSelectionSort.java
@@ -0,0 +1,8 @@
+/*The following class is a modified version of the selection sort algorithm that selects the largest 
+ * element each time and moves it to the end of the array, rather than selecting the smallest element
+ * and moving it to the beginning.
+ * Will this sort be faster than the standard selection sort, and what will the complexity class
+ * (big-Oh) be? */
+public class ModifiedSelectionSort {
+
+}
