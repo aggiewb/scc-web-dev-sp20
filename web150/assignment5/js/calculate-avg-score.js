a0e0f9650226cde4dc1b0425711a68bb29645531
AggieWB <usmaggie23@gmail.com>
2020-05-14 12:26:53 -0700

    Wrap script in an IIFE to protect code

--- web150/assignment5/js/calculate-avg-score.js
+++ web150/assignment5/js/calculate-avg-score.js
@@ -1,27 +1,29 @@
-//Function to get individual user scores, stores it in an array, and returns array.
-function getScore() {
-    var score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
-    var scores = [];
-
-    for(var i = 0; score !== 999; i++){
-        scores[i] = score;
+(function() {
+    //Function to get individual user scores, stores it in an array, and returns array.
+    function getScore() {
         var score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
+        var scores = [];
+
+        for(var i = 0; score !== 999; i++){
+            scores[i] = score;
+            var score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
+        }
+
+        return scores;
     }
 
-    return scores;
-}
+    //Function to average an array of scores
+    function avgScore(scoreArray){
+        var scoreSum = 0;
 
-//Function to average an array of scores
-function avgScore(scoreArray){
-    var scoreSum = 0;
+        for(var i = 0; i < scoreArray.length; i++){
+            scoreSum += scoreArray[i];
+        }
 
-    for(var i = 0; i < scoreArray.length; i++){
-        scoreSum += scoreArray[i];
+        var averageScore = scoreSum / scoreArray.length;
+        window.alert("Average score is " + averageScore);
     }
 
-    var averageScore = scoreSum / scoreArray.length;
-    window.alert("Average score is " + averageScore);
-}
-
-//Call avgScore and pass in the return value of getScore
-avgScore(getScore());
+    //Call avgScore and pass in the return value of getScore
+    avgScore(getScore());
+}());
