58eb1d209f910fee1140e08cbc7037c046b4dcaa
AggieWB <usmaggie23@gmail.com>
2020-05-14 12:31:49 -0700

    Update commenting to be clearer, and remove var keyword

--- web150/assignment5/js/calculate-avg-score.js
+++ web150/assignment5/js/calculate-avg-score.js
@@ -1,12 +1,12 @@
 (function() {
-    //Function to get individual user scores, stores it in an array, and returns array.
+    //Function to get individual user scores, which are stored in an array, and returns array.
     function getScore() {
         var score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
         var scores = [];
 
         for(var i = 0; score !== 999; i++){
             scores[i] = score;
-            var score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
+            score = parseInt(window.prompt("Enter test score\nOr enter 999 to end entries"));
         }
 
         return scores;
