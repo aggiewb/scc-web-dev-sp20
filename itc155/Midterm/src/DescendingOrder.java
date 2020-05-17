bd38311c07fd6a24b9c048f0e2ae8a56681080fc
AggieWB <usmaggie23@gmail.com>
2020-05-17 13:59:26 -0700

    Add comment block with problem 3 instructions

--- itc155/Midterm/src/DescendingOrder.java
+++ itc155/Midterm/src/DescendingOrder.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/*Write a void method selectionSortDescendTrace() that takes an integer array, 
+and sorts the array into descending order. The method should use nested loops 
+and output the array after each iteration of the outer loop, thus outputting 
+the array N-1 times (where N is the size). Complete main() to read in a list of 
+up to 10 positive integers (ending in -1) and then call the 
+selectionSortDescendTrace() method.*/
+
 public class DescendingOrder {
 	// TODO: Write a void method selectionSortDescendTrace() that takes
 	// an integer array and the number of elements in the array as arguments,
