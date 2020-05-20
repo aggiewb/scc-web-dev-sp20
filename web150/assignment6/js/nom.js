cd3ceb5418012e9803633afb1ebb437dee2978ca
AggieWB <usmaggie23@gmail.com>
2020-05-20 15:55:48 -0700

    Remove for loop and if/else for h3 element sets
    Change multi-line commenting with appropriate syntax
    Correct variable name stored in navbarLiElements

--- web150/assignment6/js/nom.js
+++ web150/assignment6/js/nom.js
@@ -9,13 +9,8 @@ headingSection.lastElementChild.textContent = "We're Dino-mite!";
 
 //Set h3 elements' text content
 var h3Elements = document.querySelectorAll('h3');
-for(var i = 0; i < h3Elements.length; i++){
-    if(i === 0){
-        h3Elements[0].textContent = "Hours";
-    } else if(i === 1){
-        h3Elements[1].textContent = "Location";
-    }
-}
+h3Elements[0].textContent = "Hours";
+h3Elements[1].textContent = "Location";
 
 //Get all tr element children of h3 "Hours" element to construct td elements
 var tableRows = document.getElementsByTagName('tr')
@@ -24,6 +19,9 @@ var tdElement;
 //Add first row of table data
 var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var firstTableRow = tableRows[0];
+
+/*For each day of the week, create a td element with a text node of a day of the week, 
+and append the element to the table row. */
 for(var i = 0; i < daysOfWeek.length; i++){
     tdElement = document.createElement('td');
     var tdFirstContent = document.createTextNode(daysOfWeek[i]);
@@ -34,6 +32,9 @@ for(var i = 0; i < daysOfWeek.length; i++){
 //Add second row of table data
 var hours = ["11am-8pm", "11am-10pm", "8am-3pm"];
 var secondTableRow = tableRows[1];
+
+/*For each type of hour interval, create a td element with a text node of an hour interval, 
+and append the element to the table row. Set the colspan of 3 to the first two rows only. */
 for(var i = 0; i < hours.length; i++){
     tdElement = document.createElement('td');
     var tdSecondContent = document.createTextNode(hours[i]);
@@ -46,10 +47,10 @@ for(var i = 0; i < hours.length; i++){
 
 //Remove navbar children li with a tag elements, menus, contact, and news
 var navbarElement = document.getElementsByClassName('navbar');
-var navbarLiElements = navbar[0].getElementsByTagName('li');
+var navbarLiElements = navbarElement[0].getElementsByTagName('li');
 
-//For each li element in the navbar, check to see if its child anchor tag text 
-//content is "Home", if not remove the li element.
+/*For each li element in the navbar, check to see if its child anchor tag text 
+content is "Home", if not remove the li element. */
 for(var i = 0; i < navbarLiElements.length; i++){
     var anchorElement = navbarLiElements[i].getElementsByTagName('a')[0];
     if(anchorElement.textContent !== "Home"){
