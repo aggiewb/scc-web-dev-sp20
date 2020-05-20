c9241f0dc7afe294d784794ad7519787ddca3362
AggieWB <usmaggie23@gmail.com>
2020-05-20 14:04:08 -0700

    Set h1 and h2 heading text content

--- web150/assignment6/js/nom.js
+++ web150/assignment6/js/nom.js
@@ -1,3 +1,8 @@
 //Access hidden table for hours, and remove class name to display table.
 var hidden = document.getElementsByClassName('hide');
 hidden[0].className = '';
+
+//Set h1 and h2 heading text content
+var headingSection = document.querySelector('hgroup');
+headingSection.firstElementChild.textContent = "Omnomnomnivore";
+headingSection.lastElementChild.textContent = "We're Dino-mite!";
