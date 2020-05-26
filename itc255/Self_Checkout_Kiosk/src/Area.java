a1fa2e216955c10f31421bd43e4a82069fe0e78a
AggieWB <usmaggie23@gmail.com>
2020-05-26 12:37:03 -0700

    Add private field, constructor, and getter for Area

--- itc255/Self_Checkout_Kiosk/src/Area.java
+++ itc255/Self_Checkout_Kiosk/src/Area.java
@@ -1,4 +1,20 @@
 
 public abstract class Area {
+	private double weight;
+	
+	Area(double weight){
+		this.weight = weight;
+	}
 
+	public double getWeight() {
+		return weight;
+	}
+	
+	public boolean checkWeight(double weight) {
+		if(weight == this.weight) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
