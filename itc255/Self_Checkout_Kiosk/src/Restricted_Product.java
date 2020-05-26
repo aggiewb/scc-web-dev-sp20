7d40fe661c2c8d137b936435187da3fbf6b8ea8e
AggieWB <usmaggie23@gmail.com>
2020-05-26 10:44:02 -0700

    Add private fields, constructor, and getters/setters for Restricted
    Product
    Extend Product class

--- itc255/Self_Checkout_Kiosk/src/Restricted_Product.java
+++ itc255/Self_Checkout_Kiosk/src/Restricted_Product.java
@@ -1,4 +1,35 @@
 
-public class Restricted_Product {
+public class Restricted_Product extends Product{
+	private boolean restriction;
+	private int ageLimit;
+	private int quantityLimit;
+	
+	Restricted_Product(int barcode, String name, String type) {
+		super(barcode, name, type);
+	}
+	
+	public boolean getRestriction() {
+		return restriction;
+	}
+	
+	public void setRestriction(boolean restriction) {
+		this.restriction = restriction;
+	}
+	
+	public int getAgeLimit() {
+		return ageLimit;
+	}
+	
+	public void setAgeLimit(int ageLimit) {
+		this.ageLimit = ageLimit;
+	}
 
+	public int getQuantityLimit() {
+		return quantityLimit;
+	}
+
+	public void setQuantityLimit(int quantityLimit) {
+		this.quantityLimit = quantityLimit;
+	}
+	
 }
