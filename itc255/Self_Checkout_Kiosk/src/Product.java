a4ff7017a9ee76cd2c3db63e6d0efe6a0571ebda
AggieWB <usmaggie23@gmail.com>
2020-05-26 10:29:20 -0700

    Add private fields for Product and getters/setters

--- itc255/Self_Checkout_Kiosk/src/Product.java
+++ itc255/Self_Checkout_Kiosk/src/Product.java
@@ -1,9 +1,30 @@
 
 public class Product {
-	private static int barcode;
+	private int barcode;
+	private String type;
+	private String name;
+	private int amountAvailable;
+	
+	Product(int barcode, String name, String type){
+		this.barcode = barcode;
+		this.name = name;
+		this.type = type;
+	}
 
-	public static int getProductBarcode(String productName) {
-		return barcode;
+	public int getProductBarcode(String productName) {
+		return this.barcode;
+	}
+	
+	public String getProduct() {
+		return this.name + " " + this.type;
+	}
+	
+	public void setAmountAvailable(int amount) {
+		this.amountAvailable = amount;
+	}
+	
+	public int getAmountAvailable() {
+		return this.amountAvailable;
 	}
 
 }
