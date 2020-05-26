f43607043910a13098570a86f8c9003f31855336
AggieWB <usmaggie23@gmail.com>
2020-05-26 13:04:20 -0700

    Add private field, constructor, and getter/setter for Scan
    Extend Area class

--- itc255/Self_Checkout_Kiosk/src/Scan.java
+++ itc255/Self_Checkout_Kiosk/src/Scan.java
@@ -1,4 +1,17 @@
 
 public class Scan extends Area {
+	private int barcodeId;
+	
+	Scan(int weight){
+		super(weight);
+	}
+	
+	public void setBarcodeId(int barCode) {
+		this.barcodeId = barCode;
+	}
+
+	public int getBarcodeId() {
+		return barcodeId;
+	}
 
 }
