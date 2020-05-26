293a3afe79b01b1d5a12897d20185d3d299c6ace
AggieWB <usmaggie23@gmail.com>
2020-05-26 10:38:40 -0700

    Add private fields, constructor, and getters/setters for Employee

--- itc255/Self_Checkout_Kiosk/src/Employee.java
+++ itc255/Self_Checkout_Kiosk/src/Employee.java
@@ -1,4 +1,27 @@
 
 public class Employee extends Person{
+	private int employeeId;
+	private boolean restrictedItemAuth;
+	
+	Employee(String lastName, String firstName, String email){
+		super(lastName, firstName, email);
+	}
+	
+	public void setEmployeeId(int id) {
+		this.employeeId = id;
+	}
+	
+	public int getEmployeeId() {
+		return this.employeeId;
+	}
+	
+	public void setRestrictedItemAuth(boolean auth) {
+		this.restrictedItemAuth = auth;
+	}
+	
+	public boolean getRestrictedItemAuth() {
+		return this.restrictedItemAuth;
+	}
+	
 
 }
