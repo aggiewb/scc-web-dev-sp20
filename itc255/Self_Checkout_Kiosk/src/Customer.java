c7c1239ec7898a6578592d936a6684e94f54ff4d
AggieWB <usmaggie23@gmail.com>
2020-05-26 14:19:24 -0700

    Add age field and getter/setter and add method to
    resetAssistance

--- itc255/Self_Checkout_Kiosk/src/Customer.java
+++ itc255/Self_Checkout_Kiosk/src/Customer.java
@@ -2,6 +2,7 @@
 public class Customer extends Person {
 	private int totalRewardPoints;
 	public boolean assistance = false;
+	public int age;
 	
 	Customer(String lastName, String firstName, String email){
 		super(lastName, firstName, email);
@@ -19,9 +20,21 @@ public class Customer extends Person {
 		this.assistance = true;
 	}
 	
+	public void resetAssistance() {
+		this.assistance = false;
+	}
+	
 	public double payTotalDue(Sale order, double payment) {
 		double total = order.getTotal();
 		return total - payment;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 	
 }
