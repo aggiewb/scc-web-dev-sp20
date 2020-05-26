3b8035e26c3e7ae58e12ec454ae5ca985ef82d4a
AggieWB <usmaggie23@gmail.com>
2020-05-26 10:01:57 -0700

    Add constructor for Customer class from super class
    Add totalRewardPoint field for Customer and add getters and setters

--- itc255/Self_Checkout_Kiosk/src/Customer.java
+++ itc255/Self_Checkout_Kiosk/src/Customer.java
@@ -1,4 +1,18 @@
 
 public class Customer extends Person {
-
+	private int totalRewardPoints;
+	public boolean assistance = false;
+	
+	Customer(String lastName, String firstName, String email){
+		super(lastName, firstName, email);
+	}
+	
+	public void setTotalRewardPoints(int points) {
+		this.totalRewardPoints = points;
+	}
+	
+	public int getTotalRewardPoints() {
+		return this.totalRewardPoints;
+	}
+	
 }
