f2a6a23179cb21870336f97f7a05a32dec39887f
AggieWB <usmaggie23@gmail.com>
2020-05-26 12:43:49 -0700

    Add private field, constructor, and getter/setter for Area
    Extends Area class

--- itc255/Self_Checkout_Kiosk/src/Bag.java
+++ itc255/Self_Checkout_Kiosk/src/Bag.java
@@ -1,4 +1,17 @@
 
 public class Bag extends Area {
+	private int barcodeId;
+	
+	Bag(int weight){
+		super(weight);
+	}
+	
+	public void setBarcodeId(int barCode) {
+		this.barcodeId = barCode;
+	}
+
+	public int getBarcodeId() {
+		return barcodeId;
+	}
 
 }
