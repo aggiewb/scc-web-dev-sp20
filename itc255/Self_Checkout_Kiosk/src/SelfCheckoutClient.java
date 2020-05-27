cb514b35f5073599767146648860266e2e2200b0
AggieWB <usmaggie23@gmail.com>
2020-05-27 08:00:07 -0700

    Add setting of details to newCustomer and System.out.print of
    that information

--- itc255/Self_Checkout_Kiosk/src/SelfCheckoutClient.java
+++ itc255/Self_Checkout_Kiosk/src/SelfCheckoutClient.java
@@ -3,13 +3,18 @@ public class SelfCheckoutClient {
 
 	public static void main(String[] args) {
 		newCustomer();
+		System.out.println();
+		
 	}
 	
 	public static void newCustomer() {
-		Customer bobSmith = new Customer("Smith", "Bob", "simple@example.com");
-		bobSmith.setAddress("132, My Street", "Kingston", "New York", 12401);
-		System.out.println(bobSmith);
-		System.out.println(bobSmith.getAddress());
+		Customer fakeCustomer = new Customer("Smith", "Bob", "simple@example.com");
+		fakeCustomer.setAddress("132, My Street", "Kingston", "New York", 12401);
+		fakeCustomer.setPhoneNumber("425-555-2323");
+		fakeCustomer.setAge(36);
+		fakeCustomer.setTotalRewardPoints(100);
+		System.out.println(fakeCustomer);
+		System.out.println("Age: " + fakeCustomer.getAge() + " Address: " + fakeCustomer.getAddress());
 	}
 
 }
