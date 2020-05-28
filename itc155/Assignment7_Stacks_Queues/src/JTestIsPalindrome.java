2ecbffc3fe547f318116af359642f8b0381afcf9
AggieWB <usmaggie23@gmail.com>
2020-05-28 15:58:25 -0700

    Add JUnit test case for each class to test methods

---
+++ itc155/Assignment7_Stacks_Queues/src/JTestIsPalindrome.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JTestIsPalindrome {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
