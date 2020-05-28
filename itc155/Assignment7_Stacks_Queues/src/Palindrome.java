b8b33a1370d11ab9974ffa0242543b924a49cb65
AggieWB <usmaggie23@gmail.com>
2020-05-28 15:55:45 -0700

    Add a method isPalindrome() to Palindrome class which takes a
    queue of integers as a parameter and returns true if the numbers in the queue represent a palindrome and false otherwise
    Import Queue and Stack libraries

--- itc155/Assignment7_Stacks_Queues/src/Palindrome.java
+++ itc155/Assignment7_Stacks_Queues/src/Palindrome.java
@@ -1,4 +1,29 @@
+import java.util.Queue;
+import java.util.Stack;
 
 public class Palindrome {
+	public static boolean isPalindrome(Queue<Integer> nums){
+	    Stack<Integer> temp = new Stack<>();
+	    
+	    for(int i = 0; i < nums.size(); i++){
+	        int num = nums.remove();
+	        temp.push(num);
+	        nums.add(num);
+	    }
+	    
+	    
+	    boolean palindrome = true;
+	    
+	    while(!temp.isEmpty()){
+	        int check = temp.pop();
+	        int num = nums.remove();
+	        if(check != num){
+	            palindrome = false;
+	        }
+	        nums.add(num);
+	    }
+	    
+	    return palindrome;
+	}
 
 }
