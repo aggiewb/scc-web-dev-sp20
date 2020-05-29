8af06a5b417dbc970c1831b7223f02d2fb11fc72
AggieWB <usmaggie23@gmail.com>
2020-05-29 07:34:11 -0700

    Add tests for Palindrome.isPalindrome() which includes an empty
    Queue, a Queue with elements that is true, and a Queue with elements that is false
    Add assertEquals with a call to peek() on each Queue with Elements
    Add assertEquals with a call to isEmpty() on the Queue that starts empty
    Import Queue and LinkedList libraries

--- itc155/Assignment7_Stacks_Queues/src/JTestIsPalindrome.java
+++ itc155/Assignment7_Stacks_Queues/src/JTestIsPalindrome.java
@@ -1,12 +1,41 @@
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class JTestIsPalindrome {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEmptyPalindrome() {
+		Queue<Integer> test = new LinkedList<>();
+		
+		assertEquals(true, test.isEmpty());
+		assertEquals(true, Palindrome.isPalindrome(test));
+	}
+	
+	@Test
+	void testTruePalindrome() {
+		Queue<Integer> test = new LinkedList<>();
+		int[] toSet = {3, 8, 17, 9, 17, 8, 3};
+		for(int num : toSet) {
+			test.add(num);
+		}
+		
+		assertEquals(3, test.peek());
+		assertEquals(true, Palindrome.isPalindrome(test));
+	}
+	
+	@Test
+	void testFalsePalindrome() {
+		Queue<Integer> test = new LinkedList<>();
+		int[] toSet = {5, 10, -1, 4, 3, 2, 4, -1, 10, 5};
+		for(int num : toSet) {
+			test.add(num);
+		}
+		
+		assertEquals(5, test.peek());
+		assertEquals(false, Palindrome.isPalindrome(test));
 	}
 
 }
