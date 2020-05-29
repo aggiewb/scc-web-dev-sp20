8471bd62512dfbff3c6473deaf0acb71627fca0d
AggieWB <usmaggie23@gmail.com>
2020-05-29 07:37:32 -0700

    Add tests for Stutter.stutter() which starts with an empty stack
    and a stack with elements
    Import Stack and Assertions libraries

--- itc155/Assignment7_Stacks_Queues/src/JTestIsStutter.java
+++ itc155/Assignment7_Stacks_Queues/src/JTestIsStutter.java
@@ -1,12 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.Stack;
 
 class JTestIsStutter {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEmptyStutter() {
+		Stack<Integer> toStutter = new Stack<>();
+		
+		Stutter.stutter(toStutter);
+		
+		assertEquals(true, toStutter.isEmpty());
+		assertEquals(0, toStutter.size());
+	}
+	
+	@Test
+	void testHasStackElements() {
+		Stack<Integer> toStutter = new Stack<>();
+		for(int i = 1; i < 4; i++) {
+			toStutter.push(i);
+		}
+		
+		Stack<Integer> test = new Stack<>();
+		for(int i = 1; i < 4; i++) {
+			for(int j = 0; j < 2; j++) {
+				test.push(i);
+			}
+		}
+		
+		Stutter.stutter(toStutter);
+		
+		assertEquals(test, toStutter);
+		assertEquals(3, toStutter.peek());
 	}
 
 }
