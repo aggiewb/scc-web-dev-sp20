d0af88ea028b61ac25a34fb1039a935e6164999b
AggieWB <usmaggie23@gmail.com>
2020-05-30 09:49:39 -0700

    Add tests for Sale set and get totalTax, totalDiscount, and total

--- itc255/Self_Checkout_Kiosk/src/SaleTest.java
+++ itc255/Self_Checkout_Kiosk/src/SaleTest.java
@@ -11,12 +11,57 @@ class SaleTest {
 	void testSubtotal() {
 		Product testMilk = new Product(123, "testMilk", "dairy", 2.50);
 		Product testBeef = new Product(456, "testBeef", "dairy", 9.51);
-		Sale order = new Sale(2);
-		order.setProduct(testMilk);
-		order.setProduct(testBeef);
+		Sale testOrder = new Sale(2);
+		testOrder.setProduct(testMilk);
+		testOrder.setProduct(testBeef);
 		BigDecimal testMoney = new BigDecimal(12.01).setScale(2, RoundingMode.HALF_UP);
-		order.setSubTotal();
-		assertEquals(testMoney, order.getSubTotal());
+		testOrder.setSubTotal();
+		assertEquals(testMoney, testOrder.getSubTotal());
+	}
+	
+	@Test 
+	void testTotalTax() {
+		Product testMilk = new Product(123, "testMilk", "dairy", 2.50);
+		Product testBeef = new Product(456, "testBeef", "dairy", 9.51);
+		Sale testOrder = new Sale(2);
+		testOrder.setProduct(testMilk);
+		testOrder.setProduct(testBeef);
+		BigDecimal testMoney = new BigDecimal(1.20).setScale(2, RoundingMode.HALF_UP);
+		testOrder.setSubTotal();
+		testOrder.setTotalTax();
+		assertEquals(testMoney, testOrder.getTotalTax());
+	}
+	
+	@Test 
+	void testTotalDiscount() {
+		Product testMilk = new Product(123, "testMilk", "dairy", 2.50);
+		Product testBeef = new Product(456, "testBeef", "dairy", 9.51);
+		Sale testOrder = new Sale(2);
+		Customer testCustomer = new Customer("Test", "Bob", "test@example.com");
+		testCustomer.setTotalRewardPoints(500);
+		testOrder.setProduct(testMilk);
+		testOrder.setProduct(testBeef);
+		BigDecimal testMoney = new BigDecimal(4.08).setScale(2, RoundingMode.HALF_UP);
+		testOrder.setSubTotal();
+		testOrder.setDiscount(testCustomer);
+		assertEquals(testMoney, testOrder.getDiscount());
+	}
+	
+	@Test 
+	void testTotal() {
+		Product testMilk = new Product(123, "testMilk", "dairy", 2.50);
+		Product testBeef = new Product(456, "testBeef", "dairy", 9.51);
+		Sale testOrder = new Sale(2);
+		Customer testCustomer = new Customer("Test", "Bob", "test@example.com");
+		testCustomer.setTotalRewardPoints(500);
+		testOrder.setProduct(testMilk);
+		testOrder.setProduct(testBeef);
+		BigDecimal testMoney = new BigDecimal(9.13).setScale(2, RoundingMode.HALF_UP);
+		testOrder.setSubTotal();
+		testOrder.setTotalTax();
+		testOrder.setDiscount(testCustomer);
+		testOrder.setTotal();
+		assertEquals(testMoney, testOrder.getTotal());
 	}
 
 }
