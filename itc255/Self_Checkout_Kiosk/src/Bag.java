a4ee33403b6afd8def66732092ba0299b4cc2a6f
AggieWB <usmaggie23@gmail.com>
2020-05-30 13:14:19 -0700

    Remove method as RestrictedProduct extends Product and
    alertCustomer() method for a Product be run the same if a RestictedProduct is passed in

--- itc255/Self_Checkout_Kiosk/src/Bag.java
+++ itc255/Self_Checkout_Kiosk/src/Bag.java
@@ -13,16 +13,4 @@ public class Bag extends Area {
 		}
 	}
 	
-	public String alertCustomer(RestrictedProduct item, double productWeight) {
-		if(!Area.checkWeight(item, productWeight)) {
-			String alertMessage = "Remove product from bagging area and try again.";
-			System.out.println(alertMessage);
-			return alertMessage;
-		} else {
-			String alertMessage = "Scan next item.";
-			System.out.println(alertMessage);
-			return alertMessage;
-		}
-	}
-
 }
