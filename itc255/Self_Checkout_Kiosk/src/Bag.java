d75d9321f04c2fa482863b34004aa63ac8f47339
AggieWB <usmaggie23@gmail.com>
2020-05-30 10:45:18 -0700

    Add a String return to Bag alertCustomer() method

--- itc255/Self_Checkout_Kiosk/src/Bag.java
+++ itc255/Self_Checkout_Kiosk/src/Bag.java
@@ -1,19 +1,27 @@
 
 public class Bag extends Area {
 	
-	public void alertCustomer(Product item, double productWeight) {
+	public String alertCustomer(Product item, double productWeight) {
 		if(!Area.checkWeight(item, productWeight)) {
-			System.out.println("Remove product from bagging area and try again.");
+			String alertMessage = "Remove product from bagging area and try again.";
+			System.out.println(alertMessage);
+			return alertMessage;
 		} else {
-			System.out.println("Scan next item.");
+			String alertMessage = "Scan next item.";
+			System.out.println(alertMessage);
+			return alertMessage;
 		}
 	}
 	
-	public void alertCustomer(RestrictedProduct item, double productWeight) {
+	public String alertCustomer(RestrictedProduct item, double productWeight) {
 		if(!Area.checkWeight(item, productWeight)) {
-			System.out.println("Remove product from bagging area and try again.");
+			String alertMessage = "Remove product from bagging area and try again.";
+			System.out.println(alertMessage);
+			return alertMessage;
 		} else {
-			System.out.println("Scan next item.");
+			String alertMessage = "Scan next item.";
+			System.out.println(alertMessage);
+			return alertMessage;
 		}
 	}
 
