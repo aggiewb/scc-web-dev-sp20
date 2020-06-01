9e023de042d5bf51e2717f69f59edd649b6ccb8d
AggieWB <usmaggie23@gmail.com>
2020-06-01 14:06:39 -0700

    Remove IE requirements and refactor for easier readability

--- web150/week7/example3/js/event-delegation.js
+++ web150/week7/example3/js/event-delegation.js
@@ -1,54 +1,16 @@
-function getTarget(e) {
-    if (!e) {                                      // If there is no event object
-      e = window.event;                            // Use old IE event object
+function itemDone(event) {
+    event.preventDefault();                                                 
+    var elListItem;
+
+    if (event.target.nodeName.toLowerCase() === "a") {  
+        elListItem = event.target.parentNode;                  
+    } else if (event.target.nodeName.toLowerCase() === "em") {
+        elListItem = event.target.parentNode.parentNode;            
     }
-    return e.target || e.srcElement;               // Get the target of event
-  }
-  
-  function itemDone(e) {                          
-    // Remove item from the list
-    var target, elParent, elGrandparent;           // Declare variables
-    target = getTarget(e);                         // Get the item clicked link
-  
-    /*
-    The book used the following code - but it had two shortcomings
-    - Clicking between list items would remove the whole list
-    - Clicking on italic text would remove the link - not the list item
-  
-    elParent = target.parentNode;
-    elGrandparent = target.parentNode.parentNode;
-    elGrandparent.removeChild(elParent);
-  
-    The next 10 lines improve that example
-    */
-  
-    if ( target.nodeName.toLowerCase() == "a" ) {  // If user clicked on an a element
-      elListItem = target.parentNode;              // Get its li element
-      elList = elListItem.parentNode;              // Get the ul element
-      elList.removeChild(elListItem);              // Remove list item from list
-    }
-    if ( target.nodeName.toLowerCase() == "em" ) { // If the user clicked on an em element
-      elListItem = target.parentNode.parentNode;   // Get its li element
-      elList = elListItem.parentNode;              // Get the ul element
-      elList.removeChild(elListItem);              // Remove list item from list
-    }
-  
-    // Prevent the link from taking you elsewhere
-    if (e.preventDefault) {                        // If preventDefault() works
-      e.preventDefault();                          // Use preventDefault() 
-    } else {                                       // Otherwise
-      e.returnValue = false;                       // Use old IE version
-    }
-  }
-  
-  // Set up event listeners to call itemDone() on click
-  var el = document.getElementById('shoppingList');// Get shopping list
-  if (el.addEventListener) {                       // If event listeners work
-    el.addEventListener('click', function(e) {     // Add listener on click
-      itemDone(e);                                 // It calls itemDone()
-    }, false);                                     // Use bubbling phase for flow
-  } else {                                         // Otherwise
-    el.attachEvent('onclick', function(e) {        // Use old IE model: onclick
-      itemDone(e);                                 // Call itemDone()
-    });
-  }
+
+    var elList = elListItem.parentNode;             
+    elList.removeChild(elListItem);                        
+}
+
+var el = document.getElementById('shoppingList');
+el.addEventListener('click', itemDone, false);
