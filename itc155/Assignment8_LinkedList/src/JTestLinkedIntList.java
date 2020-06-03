9d94f3c6317cfc3f82969822e23bb93d4e622f6d
AggieWB <usmaggie23@gmail.com>
2020-06-03 12:37:31 -0700

    Add JUnit class and tests(not yet implemented) for LinkedIntList
    isSorted() method

---
+++ itc155/Assignment8_LinkedList/src/JTestLinkedIntList.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JTestLinkedIntList {
+
+	@Test
+	void testEmptyTrue() {
+		fail("Not yet implemented");
+	}
+	
+	void testFullTrue() {
+		fail("Not yet implemented");
+	}
+	
+	void testFullAllEqualTrue() {
+		fail("Not yet implemented");
+	}
+	
+	void testFullFalse() {
+		fail("Not yet implemented");
+	}
+}
