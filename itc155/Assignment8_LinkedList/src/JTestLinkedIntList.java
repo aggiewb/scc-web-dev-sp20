9c548ea45b6ceb51f43d8e048336a2a57afd150b
AggieWB <usmaggie23@gmail.com>
2020-06-03 12:54:51 -0700

    Add testFullTrue() implementation with an assertTrue()

--- itc155/Assignment8_LinkedList/src/JTestLinkedIntList.java
+++ itc155/Assignment8_LinkedList/src/JTestLinkedIntList.java
@@ -10,14 +10,27 @@ class JTestLinkedIntList {
 		assertTrue(list.isSorted());
 	}
 	
+	@Test
 	void testFullTrue() {
-		fail("Not yet implemented");
+		LinkedIntList list = new LinkedIntList();
+		list.add(-3);
+		list.add(1);
+		list.add(5);
+		list.add(5);
+		list.add(8);
+		list.add(17);
+		list.add(25);
+		list.add(42);
+		list.add(87);
+		assertTrue(list.isSorted());
 	}
 	
+	@Test
 	void testFullAllEqualTrue() {
 		fail("Not yet implemented");
 	}
 	
+	@Test
 	void testFullFalse() {
 		fail("Not yet implemented");
 	}
