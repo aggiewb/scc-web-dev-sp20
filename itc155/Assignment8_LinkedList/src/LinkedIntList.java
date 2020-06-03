52ec155ec4c171defc264295024268c61cfbfcce
AggieWB <usmaggie23@gmail.com>
2020-06-03 12:47:00 -0700

    Add LinkedIntList method add() as described in chapter 16 to be
    able to add ListNodes to LinkedIntLists in JUnit test class

--- itc155/Assignment8_LinkedList/src/LinkedIntList.java
+++ itc155/Assignment8_LinkedList/src/LinkedIntList.java
@@ -6,6 +6,18 @@ public class LinkedIntList {
 		front = null;
 	}
 	
+	public void add(int value) {
+		if(front == null) {
+			front = new ListNode(value);
+		} else {
+			ListNode current = front;
+			while(current.next != null) {
+				current = current.next;
+			}
+			current.next = new ListNode(value);
+		}
+	}
+	
 	public boolean isSorted(){
 	    while(front != null && front.next != null){
 	        if(front.data > front.next.data){
