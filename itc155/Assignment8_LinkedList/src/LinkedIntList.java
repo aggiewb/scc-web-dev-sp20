6c2c24e8402d283aee25ec7831dbbd0d62536a69
AggieWB <usmaggie23@gmail.com>
2020-06-03 12:32:26 -0700

    Add method isSorted that returns true if the list is in sorted
    (nondecreasing) order and returns false otherwise

--- itc155/Assignment8_LinkedList/src/LinkedIntList.java
+++ itc155/Assignment8_LinkedList/src/LinkedIntList.java
@@ -4,5 +4,15 @@ public class LinkedIntList {
 	
 	public LinkedIntList() {
 		front = null;
-	}	
+	}
+	
+	public boolean isSorted(){
+	    while(front != null && front.next != null){
+	        if(front.data > front.next.data){
+	            return false;
+	        }
+	        front = front.next;
+	    }
+	    return true;
+	}
 }
