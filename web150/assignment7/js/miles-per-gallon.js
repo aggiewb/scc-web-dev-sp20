03c53bcfdb2dc3e9384abc6e6b9c3c648ad2a7de
AggieWB <usmaggie23@gmail.com>
2020-06-03 23:31:20 -0700

    Add a function that calculates a user's miles per gallon according
     to their form inputs
    Add an event listener on the form element for a submit event

--- web150/assignment7/js/miles-per-gallon.js
+++ web150/assignment7/js/miles-per-gallon.js
@@ -1,2 +1,13 @@
 //Disable input element so that it can be used to output miles per gallons to user
-document.getElementById("miles-per-gallons").disabled = true;
+document.getElementById('miles-per-gallon').disabled = true;
+
+//Add a function that calculates a user's miles per gallon according to their form inputs
+function calculateMilesPerGallon(event){
+    var miles = event.target.querySelector('#miles').value;
+    var gallons = event.target.querySelector('#gallons').value;
+    event.target.querySelector('#miles-per-gallon').value = miles / gallons;
+    event.preventDefault();
+}
+
+//Add an event listener on the form element for a submit event
+document.querySelector('form').addEventListener('submit', calculateMilesPerGallon, false);
