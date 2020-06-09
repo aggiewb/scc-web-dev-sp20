8afea196f047b8b9662d747b33ef7a348e1886dc
AggieWB <usmaggie23@gmail.com>
2020-06-09 14:42:31 -0700

    Add private field to IntTree class and public and private
    recursive method countEmpty()

--- itc155/Assignment9_BinaryTrees/src/IntTree.java
+++ itc155/Assignment9_BinaryTrees/src/IntTree.java
@@ -1,4 +1,16 @@
 
 public class IntTree {
+	private IntTreeNode overallRoot;
+	
+	public int countEmpty(){
+	    return countEmpty(overallRoot);
+	}
 
+	private int countEmpty(IntTreeNode root){
+	    if(root == null){
+	        return 1;
+	    } else {
+	        return countEmpty(root.left) + countEmpty(root.right);
+	    }
+	}
 }
