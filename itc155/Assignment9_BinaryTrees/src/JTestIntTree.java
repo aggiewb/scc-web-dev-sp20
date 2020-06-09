d8509de0b15a73fe7b39dd357d2b9ccf19eabb96
AggieWB <usmaggie23@gmail.com>
2020-06-09 14:47:08 -0700

    Add test methods without implementation

--- itc155/Assignment9_BinaryTrees/src/JTestIntTree.java
+++ itc155/Assignment9_BinaryTrees/src/JTestIntTree.java
@@ -5,8 +5,27 @@ import org.junit.jupiter.api.Test;
 class JTestIntTree {
 
 	@Test
-	void test() {
+	void testNullRoot() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testOnlyRoot() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testMultipleLeftEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultipleRightEmpty() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testEqualLeftRightEmpty() {
+		fail("Not yet implemented");
+	}
 }
