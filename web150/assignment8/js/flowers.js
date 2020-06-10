3c43b0cda88cf2ec6ccc927b538c963ae82149bb
AggieWB <usmaggie23@gmail.com>
2020-06-10 15:20:03 -0700

    Move order of function call

--- web150/assignment8/js/flowers.js
+++ web150/assignment8/js/flowers.js
@@ -1,9 +1,8 @@
 function intro(){
     var $newPElement = $('<p>Aggie Wheeler Bateman &copy 2020 <a href="https://www.aggiewheelerbateman.com" target="_blank">Personal Portfolio</a></p>');
-    $("h1").fadeOut(8000, "linear");
-    $("p").fadeOut(8000, "linear");
     $newPElement.hide().prependTo($("body"));
     $newPElement.show(15000, "linear");
+    $("h1, p").fadeOut(8000, "linear");
 }
 
 $("img").draggable();
