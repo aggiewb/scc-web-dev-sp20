00e724c32d6d89d280599106591ae3b720e0306b
AggieWB <usmaggie23@gmail.com>
2020-06-11 02:16:42 -0700

    Add JUnit test class with non-implemented tests for Stack class
    equals() method and LinkedIntList hasTwoConsecutive() method

---
+++ itc155/Final/src/JTestFinal.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JTestFinal {
+
+	@Test
+	void testStackEqual() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStackEmpty() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStackNotEqual() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testConsecutiveListTrue() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testConsecutiveListFalse() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testConsecutiveListOneNode() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testConsecutiveListEmpty() {
+		fail("Not yet implemented");
+	}
+}
