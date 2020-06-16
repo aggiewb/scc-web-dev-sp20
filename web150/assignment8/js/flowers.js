bd37292d69b55b95f6b508df0ba91965069b229b
AggieWB <usmaggie23@gmail.com>
2020-06-15 21:06:57 -0700

    Add a function that resets the top and left coordinate back to
    their original 0, 0 location within the #flowers div
    Add button() call in intro() which includes a fadeOut() separately for p and h1 element and a call to show() for buttonElement after the fadeOut() of h1 is complete

--- web150/assignment8/js/flowers.js
+++ web150/assignment8/js/flowers.js
@@ -3,13 +3,36 @@
 //A function that creates new p element to prependTo() the body and then show() after h1 and p tag user instructions have fadedOut().
 function intro(){
     var $newPElement = $('<p>Aggie Wheeler Bateman &copy 2020 <a href="https://www.aggiewheelerbateman.com" target="_blank">Personal Portfolio</a></p>');
+    var $buttonElement = $("button");
+    
+    $buttonElement.button();
+    $buttonElement.hide().button();
     $newPElement.hide().prependTo($("body"));
-    $("h1, p").fadeOut(8000, "linear");
+
+    //The third argument in fadeOut() is a complete parameter, which is 
+    //a function to call once the animation is complete, called once per matched element.
+    $("h1").fadeOut(8000, "linear", function(){
+        $buttonElement.show(5000, "linear");
+    });
+    $("p").fadeOut(6000, "linear");
+    
     $newPElement.show(15000, "linear");
 }
+
+//A function that resets the top and left coordinate back to their original 0, 0 location within the #flowers div
+function resetFlowers(){
+    $("button").on("click", function() {
+        $("img").each(function(){
+            this.style.top = 0;
+            this.style.left = 0;
+        })
+    });
+}
+
+
 
 //Add draggable() functionality to all images
 $("img").draggable();
 
 intro();
-
+resetFlowers();
