666b9fad7ef075e39780a4c5b722abba106900e2
AggieWB <usmaggie23@gmail.com>
2020-06-15 21:36:59 -0700

    Rename intro() to transition() which now includes removal of
    mousedown event listener
    Add elementsToHide() function
    Add mousedown click listener on #flowers div
    Adjust ms time in functions

--- web150/assignment8/js/flowers.js
+++ web150/assignment8/js/flowers.js
@@ -1,22 +1,17 @@
 //This assignment demonstrates the use of a few jQuery effects and an interaction.
 
 //A function that creates new p element to prependTo() the body and then show() after h1 and p tag user instructions have fadedOut().
-function intro(){
-    var $newPElement = $('<p>Aggie Wheeler Bateman &copy 2020 <a href="https://www.aggiewheelerbateman.com" target="_blank">Personal Portfolio</a></p>');
-    var $buttonElement = $("button");
-    
-    $buttonElement.button();
-    $buttonElement.hide().button();
-    $newPElement.hide().prependTo($("body"));
+function transition(){
+    $("#flowers").off("mousedown");
 
     //The third argument in fadeOut() is a complete parameter, which is 
     //a function to call once the animation is complete, called once per matched element.
-    $("h1").fadeOut(8000, "linear", function(){
-        $buttonElement.show(5000, "linear");
+    $("h1").fadeOut(3000, "linear", function(){
+        $("button").show(3000, "linear");
     });
-    $("p").fadeOut(6000, "linear");
+    $("p").fadeOut(3000, "linear");
     
-    $newPElement.show(15000, "linear");
+    $("#copyright").show(8000, "linear");
 }
 
 //A function that resets the top and left coordinate back to their original 0, 0 location within the #flowers div
@@ -29,10 +24,20 @@ function resetFlowers(){
     });
 }
 
+//A function that creates/hides html elements to later show during transition()
+function elementsToHide(){
+    var $newPElement = $('<p id="copyright">Aggie Wheeler Bateman &copy 2020 <a href="https://www.aggiewheelerbateman.com" target="_blank">Personal Portfolio</a></p>');
+    var $buttonElement = $("button");
+    
+    $buttonElement.button();
+    $buttonElement.hide().button();
+    $newPElement.hide().prependTo($("body"));
+}
+
 
 
 //Add draggable() functionality to all images
 $("img").draggable();
-
-intro();
+elementsToHide();
+$("#flowers").on("mousedown", transition);
 resetFlowers();
